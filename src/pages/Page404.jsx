ef37f661f51a984f2a318eb56fee4036f3790937
Ariel Barkan <abarkan@magicsoftware.com>
2022-03-15 00:38:05 +0200

    adding br

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -5,7 +5,7 @@ const Page404 = () => {
   const prevRoute = useLocation(100);
   return (
     <StyledSection>
-      404 '<u>{prevRoute.pathname}</u>' not found
+      404 <br /> '<u>{prevRoute.pathname}</u>' not found
     </StyledSection>
   );
 };
